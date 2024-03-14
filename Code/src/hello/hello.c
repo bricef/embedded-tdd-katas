fd09deaf930eb1d348d6e888fad7ca3b70f333f1
Brice Fernandes <brice@fractallambda.com>
2024-03-14 20:20:21 +0000

    blinky script

--- Code/src/hello/hello.c
+++ Code/src/hello/hello.c
@@ -12,7 +12,32 @@
     #include "pico/stdlib.h"
 #endif
 
+
+#define RGB_MASK 0b00000010000000110000000000000000
+#define RED      17
+#define GREEN    16
+#define BLUE     25
+#define LOW false
+#define HIGH true
+
 int main() {
     stdio_init_all();
     printf("Hello, world!\n");
+    gpio_init_mask(RGB_MASK);
+    gpio_set_dir_out_masked(RGB_MASK);
+    gpio_set_mask(RGB_MASK);
+    while (true){
+        gpio_put(RED, LOW);
+        sleep_ms(500);
+        gpio_put(RED, HIGH);
+        
+        gpio_put(GREEN, LOW);
+        sleep_ms(500);
+        gpio_put(GREEN, HIGH);
+
+        gpio_put(BLUE, LOW);
+        sleep_ms(500);
+        gpio_put(BLUE, HIGH);
+    }
+
 }
